09e2e40f12d478a4f6be406603ede8faeb04ae46
Vbeni <benivillafana@gmail.com>
2023-07-15 11:32:09 -0400

    log in component

---
+++ src/components/Users/Login.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+
+const Login = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = (event) => {
+    event.preventDefault();
+
+    setUsername('');
+    setPassword('');
+  };
+
+  return (
+    <form onSubmit={handleLogin}>
+      <label>
+        Username:
+        <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
+      </label>
+      <label>
+        Password:
+        <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+      </label>
+      <button type="submit">Login</button>
+    </form>
+  );
+};
+
+export default Login;
