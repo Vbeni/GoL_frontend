6227600d2e5fd0c036ab1fc5bc69e2e777c2b1c1
Vbeni <benivillafana@gmail.com>
2023-07-17 12:04:59 -0400

    add on click to cell.js

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import '../App.css';
 //resusable cell component w/isAlive prop
-function Cell({ isAlive }) {
+function Cell({ isAlive, onClick }) {
   return (
     //applies alive or dead class to cell
-    <div className={`cell ${isAlive ? "alive" : "dead"}`} />
+    <div className={`cell ${isAlive ? "alive" : "dead"}`} onClick={onClick} />
   );
 }
 
