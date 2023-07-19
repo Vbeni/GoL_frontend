94d9e067369f8ca6219edfc4fd2aefaed49b875d
Vbeni <benivillafana@gmail.com>
2023-07-19 12:31:55 -0400

    cell.js notes/code cleaning

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import '../App.css';
-//resusable cell component w/isAlive prop
+//resusable cell component w/3 props
 function Cell({ isAlive, onClick, cellSize }) {
+  //cellSize prop defines width/height of cell
   const cellStyle = {
     width: `${cellSize}px`,
     height: `${cellSize}px`
   };
+  //renders a cell as a div 
+  //classname changes based on alive or dead state 
+  //style is set based on cellStyle
+  //if cell is clicked , onClick is called to change cell state 
   return (
-    //applies alive or dead class to cell
-    <div className={`cell ${isAlive ? "alive" : "dead"}`} 
+    <div 
+    className={`cell ${isAlive ? "alive" : "dead"}`} 
     style={cellStyle}
-    onClick={onClick} />
+    onClick={onClick} 
+    />
   );
 }
 
