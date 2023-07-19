80f8d0c30cf0b88a20eac2c016dc1dd5e7c2b988
Vbeni <benivillafana@gmail.com>
2023-07-19 12:44:46 -0400

    Header code cleaning and description

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import '../styles/Footer.css';
 
+//footer component 
 const Footer = () => {
     return (
         <div className="footer">
             <p>
                 Conway's Game of Life, is a cellular automaton devised by the British mathematician John Horton Conway in 1970.
-                Learn more on <a href="https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life" target="_blank" rel="noopener noreferrer">Wikipedia</a>.
+                Learn more on 
+                <a href="https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life" target="_blank" rel="noopener noreferrer">Wikipedia</a>.
             </p>
             <p>&copy; {new Date().getFullYear()} BeniV. All rights reserved.</p>
         </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,9 +2,13 @@ import React from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import CreatePattern from '../components/Patterns/CreatePattern';
 
-//Header component with 3 props
+//Header component with 4 props:
+// - showCreate: A boolean to decide whether the CreatePattern component should be shown or not.
+// - setShowCreate: A function to toggle the visibility of CreatePattern.
+// - onNewPattern: A function that's likely being passed down to handle some logic when a new pattern is created.
+// - loggedInUser: The current logged-in user's name
 const Header = ({ showCreate, setShowCreate, onNewPattern, loggedInUser }) => {
-  //current url to set specific links in header
+  // hook provides info about the current URL
   const location = useLocation();
 
   //conditionally renders showCreate if on /patternlist
@@ -15,15 +19,15 @@ const Header = ({ showCreate, setShowCreate, onNewPattern, loggedInUser }) => {
       </h1>
       <nav className="nav">
       {loggedInUser && <span>Welcome, {loggedInUser}!</span>}
-        {loggedInUser && location.pathname !== '/patternlist' &&
-          <Link to="/patternlist" className="nav-link">Pattern List</Link>
-        }
-        <Link to="/game" className="nav-link">Play Game</Link>
+      {loggedInUser && location.pathname !== '/patternlist' &&
+        <Link to="/patternlist" className="nav-link">Pattern List</Link>
+      }
+      <Link to="/game" className="nav-link">Play Game</Link>
       </nav>
       {loggedInUser && location.pathname === '/patternlist' &&
         <div className="form-container">
           <button onClick={() => setShowCreate(!showCreate)} className="nav-button">Create Pattern</button>
-          {showCreate && <CreatePattern afterSubmit={() => {setShowCreate(false); onNewPattern();}} />}
+        {showCreate && <CreatePattern afterSubmit={() => {setShowCreate(false); onNewPattern();}} />}
         </div>
       }
     </header>
