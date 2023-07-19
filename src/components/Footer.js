8c87c108a409fda5a2248aea1b873c144c6267fd
Vbeni <benivillafana@gmail.com>
2023-07-18 20:20:33 -0400

    footer css

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import '../styles/Footer.css';
 
 const Footer = () => {
     return (
         <div className="footer">
             <p>
-                Conway's Game of Life, also known as the "Game of Life", is a cellular automaton devised by the British mathematician John Horton Conway in 1970.
+                Conway's Game of Life, is a cellular automaton devised by the British mathematician John Horton Conway in 1970.
                 Learn more on <a href="https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life" target="_blank" rel="noopener noreferrer">Wikipedia</a>.
             </p>
             <p>&copy; {new Date().getFullYear()} BeniV. All rights reserved.</p>
