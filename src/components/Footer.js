6769a2e6b60c08ab3fdc5d572b6d21e1cbd58a42
Vbeni <benivillafana@gmail.com>
2023-07-19 14:08:07 -0400

    game.js notes and cleaning

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,7 +8,7 @@ const Footer = () => {
             <p>
                 Conway's Game of Life, is a cellular automaton devised by the British mathematician John Horton Conway in 1970.
                 Learn more on 
-                <a href="https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life" target="_blank" rel="noopener noreferrer">Wikipedia</a>.
+                <a href="https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life" target="_blank" rel="noopener noreferrer"> Wikipedia</a>.
             </p>
             <p>&copy; {new Date().getFullYear()} BeniV. All rights reserved.</p>
         </div>
--- src/pages/Game.js
+++ src/pages/Game.js
@@ -6,46 +6,71 @@ import GameGrid from '../components/Games/GameGrid';
 import '../App.css';
 
 const Game = () => {
-    // STATES
+    // STATES:
+    //size of gamegrid 
+    //grid is entire grid with cell states 
+    //isRunning to indicate of game is running or paused 
+    //generations tracks number of iterations since game start 
     const [size, setSize] = useState(25);
     const [grid, setGrid] = useState([]);
     const [isRunning, setIsRunning] = useState(false);
     const [generationsCount, setGenerationsCount] = useState(0);
 
-    // GRID INITIALIZATION AND COMPUTATION
+    // GRID INITIALIZATION AND COMPUTATION:
+    //generates empty(false) grid based on current size. Called when game is initialized or cleared 
     const generateEmptyGrid = useCallback(() => 
         Array.from({length: size}, () => Array(size).fill(false)), [size]
     );
-
+    //state of next grid based on Game of Life rule set
+    //usecallback so grid only rerenders on size change 
     const computeNextGrid = useCallback((oldGrid) => {
+        //nested map to calculate state of each oldgrid cell in next generation 
         return oldGrid.map((row, i) => row.map((cell, j) => {
+        //nested array iterations to calculate neighbors of cell using offsets -1, 0, 1
+        //flatmap flattens array of arrays into 2d array 
             const neighbors = [-1, 0, 1].flatMap(dx => 
                 [-1, 0, 1].map(dy => {
+                    //checks if current cell, ignores it 
                     if (dx === 0 && dy === 0) return 0;
+                    //flatmap returns single values for dx & dy instead of arrays of 3 values 
                     const x = i + dx;
                     const y = j + dy;
-                    return x >= 0 && x < size && y >= 0 && y < size && oldGrid[x][y] ? 1 : 0;
-                })
-            ).reduce((a, b) => a + b);
 
+                    //checks if row index of neighbors is within grid boundaries 
+                    return x >= 0 && x < size 
+                    //checks if column index within boundaries 
+                    && y >= 0 && y < size 
+                    //checks if neighbor cell (x,y) is alive 
+                    && oldGrid[x][y] ? 1 : 0;
+                    //if conditions true then neighbor cell is alive and within grid boundaries so return 1
+                })
+                //reduce to accumulate total number of live nieghbors 
+            ).reduce((a, b) => a + b);
+        //live cell with 2 or 3 neighbors survives 
+        //any dead cell with 3 neighbors becomes live cell 
             return neighbors === 3 || (cell && neighbors === 2);
         }));
     }, [size]);
-
+    
+    //initializes grid whenever size changes 
     useEffect(() => {
         setGrid(generateEmptyGrid());
     }, [size, generateEmptyGrid]);
 
 
-    // HANDLERS
+    // HANDLERS:
+
+    //toggles state of cell when clicked 
     const handleCellClick = useCallback((i, j) => {
         setGrid(grid => grid.map((row, x) => row.map((cell, y) => x === i && y === j ? !cell : cell)));
     }, []);
-
+    
+    //toggles pause/play
     const handlePlay = useCallback(() => {
         setIsRunning(isRunning => !isRunning);
     }, []);
-
+    
+    //stops game and clears grid 
     const handleClear = useCallback(() => {
         setIsRunning(false);
         setGrid(generateEmptyGrid());
@@ -54,6 +79,7 @@ const Game = () => {
 
 
     // GAME LOGIC
+    //computes next grid state & updates generations count every 700ms when game is running 
     useEffect(() => {
         if (!isRunning) return;
         const interval = setInterval(() => {
@@ -63,8 +89,9 @@ const Game = () => {
         return () => clearInterval(interval);
     }, [isRunning, computeNextGrid]);
 
-
+    //number of living cells in current grid 
     const livingCellsCount = grid.flat().filter(cell => cell).length;
+    //size of each cell in grid
     const cellSize = 500 / size;
 
     // RENDER
