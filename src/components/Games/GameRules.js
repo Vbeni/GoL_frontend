dc21c0e175140cd84b3f033e3d3de53526b948c4
Vbeni <benivillafana@gmail.com>
2023-07-18 21:59:08 -0400

    gamerules css

--- src/components/Games/GameRules.js
+++ src/components/Games/GameRules.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import '../../styles/GameRules.css'
 
 const GameRules = () => {
     const [isModalOpen, setModalOpen] = useState(false);
@@ -8,16 +9,20 @@ const GameRules = () => {
     };
 
     return (
-        <div>
-            <button onClick={toggleModal}>Rules</button>
+        <div className="rules-container">
+            <button className="button" onClick={toggleModal}>Rules</button>
 
             {isModalOpen && (
                 <div className="modal">
-                    {
-                        'rules n stuff'
-                    }
+                    <h2>Conway's Game of Life</h2>
+                    <ul>
+                        <li>Any live cell with two or three live neighbors survives.</li>
+                        <li>Any dead cell with three live neighbors becomes a live cell.</li>
+                        <li>All other live cells die in the next generation.</li>
+                    </ul>
+                    <button className="button close-button" onClick={toggleModal}>Close</button>
                 </div>
-            )}.
+            )}
         </div>
     );
 }
