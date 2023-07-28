6f67f2aaf12fae232ab57b1c1ec33e61ca8c263e
Vbeni <benivillafana@gmail.com>
2023-07-28 11:47:09 -0700

    random seeder slider & css

--- src/components/Games/RandomSeeder.js
+++ src/components/Games/RandomSeeder.js
@@ -1,17 +1,13 @@
 import React, { useState, useEffect } from 'react';
+import Slider from 'react-input-slider';
+import '../../styles/RandomSeeder.css'
 
 const RandomSeeder = ({ gridSize, onRandomSeed }) => {
   const [numCellsToSeed, setNumCellsToSeed] = useState(10); // Default number of cells to seed
 
   useEffect(() => {
-
     setNumCellsToSeed(10);
   }, [gridSize]);
-
-  const handleNumCellsChange = (event) => {
-    const value = event.target.value;
-    setNumCellsToSeed(Math.min(Math.max(parseInt(value), 0), gridSize * gridSize)); 
-  };
 
   const handleSeedCells = () => {
     onRandomSeed(numCellsToSeed);
@@ -19,16 +15,32 @@ const RandomSeeder = ({ gridSize, onRandomSeed }) => {
 
   return (
     <div className="random-seeder">
-      <label htmlFor="numCellsToSeed">Number of Cells to Seed:</label>
-      <input
-        type="number"
-        id="numCellsToSeed"
-        min="0"
-        max={gridSize * gridSize}
-        value={numCellsToSeed}
-        onChange={handleNumCellsChange}
-      />
-      <button onClick={handleSeedCells}>Seed Cells</button>
+      <label className="label">Number of Cells to Seed:</label>
+      <div className="slider-container">
+        <Slider
+          axis="x"
+          x={numCellsToSeed}
+          xmin={0}
+          xmax={gridSize * gridSize}
+          xstep={1}
+          onChange={({ x }) => setNumCellsToSeed(x)}
+          styles={{
+            active: {
+              background: '#007bff',
+            },
+            thumb: {
+              width: 12,
+              height: 12,
+              borderRadius: 6,
+              backgroundColor: '#007bff',
+            },
+          }}
+        />
+        <div className="value">{numCellsToSeed}</div>
+      </div>
+      <button onClick={handleSeedCells} className="button">
+        Seed Cells
+      </button>
     </div>
   );
 };
